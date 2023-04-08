5c40c67e0f98da3377675be068b44e7ed31e1ee9
KiranMItagi <kiranitagi01@gmail.com>
2023-04-09 00:55:02 +0530

    push

---
+++ Sample.java
@@ -0,0 +1,20 @@
+class Sample
+
+{
+	public static void main(String[] args) 
+	{
+		char[]arr=new char[4];
+		arr[0]='A';
+		arr[1]='B';
+		arr[2]='C';
+		arr[3]='D';
+		System.out.println("*********");
+		System.out.println("index \t values");
+		System.out.println("*********");
+		for(int i=0; i<=arr.length; i++)
+		{
+		
+		 System.out.println(i+"\t"+arr[i]);
+		}
+	}
+}
